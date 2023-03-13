d94a01e84dcf2ab3f38cca05c51b8b7a6e448504
jamiemcglone <jamie.mcglone21@btinternet.com>
2023-03-13 19:21:02 +0000

    :construction: updating to gh pages settings

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -8,16 +8,19 @@ const About = () => {
     <AboutContainer>
         <AboutTitle>About</AboutTitle>
     <AboutText>
-    I graduated from CodeClan's professional software development course in February 2023.
-    My previous background was in music and I graduated with a first class Bachelor of Arts with Honours degree in Popular Music in summer 2022.
-    My passion for music production developed into a curiosity to find out how the software I was constantly using was actually created, 
-    and so in June 2022 I began self studying programming.
-    I thoroughly enjoy the problem solving and creativity that programming requires, and so I pursued it beyond self study.
-    I enrolled in CodeClan and began in October 2022. It was a hugely rewarding course and since graduating, I am now excited to find my first junior role.
+    I am a software developer based in Edinburgh, Scotland. I love problem solving and challenging myself 
+    and this is something I strive to do through coding.
+    I graduated from CodeClan's professional software development course in February 2023. On this course I studied Python, Javascript and Java, 
+    and thoroughly enjoyed my time working with my fellow students.
     </AboutText>
     <AboutText>
-    Outside of programming, my main passion is music. I love listening to it, and I listen to most genres.
-    Additionally, I play guitar in a function band that gigs in various venues across Edinburgh.
+    My previous background was in music and retail. I graduated with a first class Bachelor of Arts with Honours degree in Popular Music in summer 2022.
+    My passion for music production developed into a curiosity to find out how the software I was constantly using was actually created, 
+    and so in the summer of 2022 I began self studying programming.
+    </AboutText>
+    <AboutText>
+    Outside of programming, my main passion is music. I love listening to it and playing it, and I enjoy most genres too.
+    I play guitar in a function band that gigs in various venues across Edinburgh.
     I also love sports, particularly football, which I regularly play at five a sides each week.
     </AboutText>
     </AboutContainer>
@@ -29,7 +32,7 @@ export default About;
 const AboutText = styled.p`
     font-size: 1.2rem;
     width: 60%;
-    text-align: center;
+    text-align: left;
     `
 
 const AboutTitle = styled.h1`
--- src/PortfolioContainer.tsx
+++ src/PortfolioContainer.tsx
@@ -3,6 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import About from "./Components/About";
 import Home from "./Components/Home";
 import Projects from "./Components/Projects";
+import styled from "styled-components";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MailIcon from '@mui/icons-material/Mail';
 
 const PortfolioContainer = () => {
     return <>
@@ -12,7 +16,19 @@ const PortfolioContainer = () => {
         <Route path="/projects" element={<Projects/>}/>
         <Route path="/about" element={<About/>}/>
     </Routes>
+    <Footer id="footer">
+        © Jamie McGlone {new Date().getFullYear()}
+    </Footer>
     </>
 }
 
 export default PortfolioContainer;
+
+const Footer = styled.span`
+  position: absolute;
+  bottom: 1rem;
+  left: 0;
+  right: 0;
+  text-align: center;
+  gap: 1rem;
+`

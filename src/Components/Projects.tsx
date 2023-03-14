1768f75e2fe4dfd2436a14ed1ae85c76771cd44a
jamiemcglone <jamie.mcglone21@btinternet.com>
2023-03-14 17:11:35 +0000

    :sparkles: implemented project button switcher

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
 
 const Projects = () => {
-    return <></>
+
+    const myProjects: Array<string> = ["Learnr", "Space Explorers", "Songaniser", "Blackjack Terminal Game"]
+
+
+    const [projectIndex, setProjectIndex] = useState(0);
+
+    const handleNextProjectChangeClick = () => {
+        setProjectIndex(projectIndex + 1);
+    }
+
+    const handlePreviousProjectChangeClick = () => {
+        setProjectIndex(projectIndex - 1);
+    }
+
+    return <>
+        <p>{myProjects[projectIndex]}</p>
+        {projectIndex > 0? <button onClick={handlePreviousProjectChangeClick}>Previous project!</button> : null}
+        {projectIndex + 1 < myProjects.length? <button onClick={handleNextProjectChangeClick}>Next Project!</button> : null}
+    </>
 }
 
 export default Projects;

443e66f2b46b9bbb7982a64ed9722116283abf24
jamiemcglone <jamie.mcglone21@btinternet.com>
2023-03-14 18:06:20 +0000

    :construction: working on the projects page

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,8 +1,23 @@
 import { useState } from "react";
+import styled from "styled-components";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+
+
+type Project = {
+    title: string;
+    image: string;
+    bio: string;
+}
 
 const Projects = () => {
 
-    const myProjects: Array<string> = ["Learnr", "Space Explorers", "Songaniser", "Blackjack Terminal Game"]
+    const learnr: Project = {title: "Learnr", image: "", bio: "a VLE"}
+    const spaceExplorers: Project = {title: "Space Explorers", image: "https://user-images.githubusercontent.com/65739239/212085496-2725a4c3-2861-4c87-9af2-782e56b7c3c9.png", bio: "space woooo"}
+    const songaniser: Project = {title: "Songaniser", image: "https://github.com/jamiemcglone/Songaniser-Project/raw/main/Songaniser%20Home%20Page.png", bio: "organise songs"}
+    const blackjack: Project = {title: "BlackJack Terminal Game", image: "www.fakeimage.com", bio: "fun terminal game"}
+
+    const myProjects: Array<Project> = [learnr, spaceExplorers, songaniser, blackjack]
 
 
     const [projectIndex, setProjectIndex] = useState(0);
@@ -15,11 +30,48 @@ const Projects = () => {
         setProjectIndex(projectIndex - 1);
     }
 
-    return <>
-        <p>{myProjects[projectIndex]}</p>
-        {projectIndex > 0? <button onClick={handlePreviousProjectChangeClick}>Previous project!</button> : null}
-        {projectIndex + 1 < myProjects.length? <button onClick={handleNextProjectChangeClick}>Next Project!</button> : null}
-    </>
+    return (
+    <ProjectPageContainer>
+        <ProjectInformationContainer>
+            <h3>{myProjects[projectIndex].title}</h3>
+            <ProjectImage src={myProjects[projectIndex].image}/>
+            <p>{myProjects[projectIndex].bio}</p>
+        </ProjectInformationContainer>
+        <ProjectPageButtons>
+            {projectIndex > 0? <NavigationButton onClick={handlePreviousProjectChangeClick}><ArrowBackIosNewIcon /></NavigationButton> : null}
+            {projectIndex + 1 < myProjects.length? <NavigationButton onClick={handleNextProjectChangeClick}><ArrowForwardIosIcon /></NavigationButton> : null}
+        </ProjectPageButtons>
+    </ProjectPageContainer>
+    )
 }
 
 export default Projects;
+
+const NavigationButton = styled.div`
+    :hover {
+        cursor: pointer;
+    }
+`
+const ProjectPageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+const ProjectPageButtons = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+`
+
+const ProjectInformationContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const ProjectImage = styled.img`
+    width: 50%;
+    @media (max-width: 700px) {
+        width: 95%;
+    }
+`
